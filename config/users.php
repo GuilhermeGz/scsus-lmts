881dd970aa6173bc954042fb9ed9ee6b638770b8
Edgarvital <edgar.tecnico@hotmail.com>
2022-03-27 21:29:42 -0300

    Removendo o segundo usuário

--- config/users.php
+++ config/users.php
@@ -8,11 +8,4 @@ $sys_log[0]['perfil'] = "indicadores";
 $sys_log[0]['cnes'] = 0;
 $sys_log[0]['ine'] = 0;
 $sys_log[0]['tema'] = "padrao";
-//Outro Login
-$sys_log[1]['login'] = "admin2";
-$sys_log[1]['senha'] = "123";
-$sys_log[1]['perfil'] = "indicadores";
-$sys_log[1]['cnes'] = 0;
-$sys_log[1]['ine'] = 0;
-$sys_log[1]['tema'] = "padrao";
 ?>
